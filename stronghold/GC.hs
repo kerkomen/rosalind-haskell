a7f6f80a9c189cc49361ce73a4b67447e98b3e3d
kerkomen <@>
2015-03-31 01:33:43 +0300

    The fourth problem has been solved.

---
+++ stronghold/GC.hs
@@ -0,0 +1,42 @@
+#!/usr/bin/env runhaskell
+
+-- | Complementing a Strand of DNA
+-- Usage: REVC <dataset.txt>
+
+import System.Environment(getArgs)
+import qualified Data.ByteString.Char8 as C
+import Data.Ord(comparing)
+import Data.List(sortBy)
+
+-- redundant
+
+parseFasta :: C.ByteString -> [(C.ByteString, C.ByteString)]
+parseFasta = removeKet' . pairs . C.lines
+
+removeKet' :: [(C.ByteString, t)] -> [(C.ByteString, t)]
+removeKet' = map (\(name, seq) -> (removeKet . head . C.words $ name, seq))
+
+removeKet :: C.ByteString -> C.ByteString
+removeKet  = C.dropWhile (=='>')
+
+--
+
+parseFastaMultiline :: C.ByteString -> [(C.ByteString, C.ByteString)]
+parseFastaMultiline f = zip (map (C.takeWhile (/='\n')) xs) (map (C.filter (/='\n') . C.dropWhile (/='\n')) xs)
+	where xs = filter (\x -> C.length x > 0) . C.split '>' $ f
+
+gcContent :: Fractional a => C.ByteString -> a
+gcContent seq = (C.foldr ((+) . ifS) 0.0 seq) / (fromIntegral $ C.length seq)
+	where ifS x = if x == 'C' || x == 'G' then 1 else 0
+
+pairs :: [t] -> [(t, t)]
+pairs []       = []
+pairs (x:[])   = []  -- abnormal case
+pairs (x:y:xs) = (x, y) : pairs xs
+
+main = do
+	(file:_) <- getArgs
+	fasta <- C.readFile file
+	let result = last . sortBy (comparing $ gcContent . snd) . parseFastaMultiline $ fasta
+	C.putStrLn $ fst result
+	putStrLn $ show $ 100.0 * (gcContent . snd $ result)
